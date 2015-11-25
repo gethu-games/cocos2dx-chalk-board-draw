a12a3312401506ba3aaac12181be6702e04fd869
saiy2k <saiy2k@gmail.com>
2015-11-25 12:29:51 +0530

    Erasing

--- src/Board.js
+++ src/Board.js
@@ -11,6 +11,8 @@ var Board = cc.Scene.extend({
 
     prevPoint                   :   cc.p(0, 0),
 
+    mode                        :   0,
+
     ctor                        :   function () {
 
         this._super();
@@ -43,7 +45,7 @@ var Board = cc.Scene.extend({
         this.addChild(this.renderTex);
 
         this.chalkSprite        =   cc.Sprite.create(res.chalk_png);
-        this.chalkSprite.setPosition(cc.p(100, 200));
+        this.chalkSprite.setPosition(cc.p(size.width * 0.7, this.chalkSprite.getContentSize().height * 0.8));
         this.addChild(this.chalkSprite, 2);
 
         this.duster             =   cc.Sprite.create(res.duster_png);
@@ -56,8 +58,22 @@ var Board = cc.Scene.extend({
     onTouchesBegan:function(touches, event) {
         var slf                 =   event.getCurrentTarget();
         var pos                 =   touches[0].getLocation();
+        console.log('Began : ' + JSON.stringify(pos));
         
         slf.prevPoint           =   pos;
+        slf.mode                =   0;
+
+        /*
+        var dist                =   Math.round(cc.pDistance(pos, slf.chalkSprite.getPosition()));
+        if (dist < slf.chalkSprite.getContentSize().width) {
+            slf.mode            =   0;
+        }
+        */
+
+        dist                    =   Math.round(cc.pDistance(pos, slf.duster.getPosition()));
+        if (dist < slf.duster.getContentSize().width) {
+            slf.mode            =   1;
+        }
 
         return                      true;
     },
@@ -65,7 +81,7 @@ var Board = cc.Scene.extend({
     onTouchesMoved:function(touches, event) {
         var slf                 =   event.getCurrentTarget();
         var pos                 =   touches[0].getLocation();
-        //console.log('Move : ' + JSON.stringify(pos));
+        console.log('Move : ' + JSON.stringify(pos));
         //console.log('PrevPoint : ' + JSON.stringify(slf.prevPoint));
 
         var dist                =   Math.round(cc.pDistance(pos, slf.prevPoint));
@@ -73,33 +89,64 @@ var Board = cc.Scene.extend({
 
         for (var i = 0; i < dist; i += 5) {
             var cPos            =   cc.pLerp(slf.prevPoint, pos, i/dist);
-            slf.drawBrushAtPoint(cPos, cc.color(240, 240, 240, 230));
+            if (slf.mode == 0) {
+                slf.drawBrushAtPoint(cPos, cc.color(240, 240, 240, 230));
+            } else if (slf.mode == 1) {
+                slf.eraseAtPoint(cPos);
+            }
         }
 
         slf.prevPoint           =   pos;
     },
 
     onTouchesEnded              :   function(touches, event) {
+        var slf                 =   event.getCurrentTarget();
         var pos                 =   touches[0].getLocation();
+        var size                =   cc.winSize;
+
         console.log('End : ' + JSON.stringify(pos));
+
+        if (slf.mode == 0) {
+            slf.chalkSprite.runAction(cc.moveTo(0.2, cc.p(size.width * 0.7, slf.chalkSprite.getContentSize().height * 0.8)).easing(cc.easeIn(1.0)));
+        } else if (slf.mode == 1) {
+            slf.duster.runAction(cc.moveTo(0.2, cc.p(slf.duster.getContentSize().width * 0.5, slf.duster.getContentSize().height * 0.8)).easing(cc.easeIn(1.0)));
+        }
+                //this.duster.runAction(cc.moveTo(0.1, this.erasePoint).easing(cc.easeIn(1.0)));
 
     },
 
-drawBrushAtPoint            :   function(pt, color) {
+    drawBrushAtPoint            :   function(pt, color) {
 
-    this.renderTex.begin();
-    this.chalkBrush         =   cc.Sprite.create(res.chalkBrush_png);
-    this.chalkBrush.setRotation(Math.random() * 180);
-    this.chalkBrush.setPosition(pt);
-    this.chalkBrush.setColor(color);
-    this.chalkBrush.setScale(1.5);
-    this.chalkBrush.visit();
-    this.renderTex.end();
+        this.renderTex.begin();
+        this.chalkBrush         =   cc.Sprite.create(res.chalkBrush_png);
+        this.chalkBrush.setRotation(Math.random() * 180);
+        this.chalkBrush.setPosition(pt);
+        this.chalkBrush.setColor(color);
+        this.chalkBrush.setScale(1.5);
+        this.chalkBrush.visit();
+        this.renderTex.end();
 
-    this.chalkSprite.setPosition(cc.p(pt.x - this.chalkSprite.getContentSize().width * 0.3,
-        pt.y - this.chalkSprite.getContentSize().height * 0.2));
+        this.chalkSprite.setPosition(cc.p(pt.x - this.chalkSprite.getContentSize().width * 0.3,
+            pt.y - this.chalkSprite.getContentSize().height * 0.2));
 
-}
+    },
+
+    eraseAtPoint                :   function(pt) {
+
+        this.renderTex.begin();
+        this.chalkBrush         =   cc.Sprite.create(res.chalkBrush_png);
+        this.chalkBrush.setRotation(Math.random() * 180);
+        this.chalkBrush.setPosition(pt);
+        this.chalkBrush.setScale(10.0);
+        this.chalkBrush.setOpacity(230);
+        this.chalkBrush.setBlendFunc(  cc.ZERO, cc.ONE_MINUS_SRC_ALPHA );
+        this.chalkBrush.visit();
+        this.renderTex.end();
+
+        this.duster.setPosition(pt);
+
+    }
+
 
 });
 
